dab067a55a747a627be679f92df44a7f40befcd9
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-10-11 09:24:32 -0300

    feat: define initial commands

---
+++ README.md
@@ -0,0 +1,3 @@
+## To-dos
+
+- [ ] Possible http wrapper: <https://github.com/dmmulroy/ocaml-playlist-transfer/blob/main/lib/shared/http.ml>
--- lib/commands/commands.ml
+++ lib/commands/commands.ml
@@ -1,19 +1,37 @@
-open Cmdliner
+let mount_cmd ~doc ~name ~cmd =
+  let open Cmdliner in
+  let info = Cmd.info name ~doc in
+  let term = Term.(const cmd $ const ()) in
+  Cmd.v info term
 
+let pr_title () = print_endline "pr_title command"
+let status () = print_endline "status command"
+let view () = print_endline "view command"
+let open_card () = print_endline "open_card command"
 let progress () = print_endline "progress command"
-
-let progress_cmd =
-        let doc = "This command move a particular jira card to the progress
-        column and create a new git branch using the correct pattern" in
-        let info = Cmd.info "progress" ~doc in
-        let term = Term.(const progress $ const ()) in
-        Cmd.v info term
-
 let review () = print_endline "review command"
 
+let pr_title_cmd =
+  mount_cmd
+    ~doc:
+      "This command print out the title of the current card following a github \
+       pull request pattern"
+    ~name:"pr_title" ~cmd:pr_title
+
+let status_cmd =
+  mount_cmd ~doc:"This command print out which column the current card is"
+    ~name:"status" ~cmd:status
+
+let open_card_cmd =
+  mount_cmd ~doc:"This command open the current card on the available browser"
+    ~name:"open" ~cmd:open_card
+
+let progress_cmd =
+  mount_cmd
+    ~doc:"This command move a particular jira card to\nthe progress column"
+    ~name:"progress" ~cmd:progress
+
 let review_cmd =
-        let doc = "This command move a particular jira card to the review
-        column and create a new github pull request using the gh cli tool" in
-        let info = Cmd.info "review" ~doc in
-        let term = Term.(const review $ const ()) in
-        Cmd.v info term
+  mount_cmd
+    ~doc:"This command move a particular jira card to\nthe review column"
+    ~name:"review" ~cmd:review
