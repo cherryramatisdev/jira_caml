d29a1a20272903492c5e989c806f3ed81057ddd8
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-10-15 19:17:36 -0300

    feat(jira): define jira module
    Added a function to return the description of a particular card from
    jira

--- lib/jira.ml
+++ lib/jira.ml
@@ -1,11 +1,19 @@
-open Lwt
-open Cohttp_lwt
-
-let () =
-  let headers = Header.init () in
-  let* response =
-    Lwt.return
-      (Client.get ~headers
-         (Uri.of_string "https://jsonplaceholder.typicode.com/posts/1"))
+let get_card_content code =
+  let open Shared in
+  let open Cohttp in
+  let* config = Config.config () in
+  let headers =
+    Header.init () |> fun h ->
+    Header.add_authorization h
+      (`Basic (config.auth.user_mail, config.auth.user_token))
   in
-  print_endline response
+  let url =
+    Printf.sprintf "%s/rest/api/2/issue/%s-%s" config.prefixes.url_prefix
+      config.prefixes.card_prefix code
+  in
+  let open Yojson.Safe.Util in
+  let description =
+    Http.get ~headers url |> Yojson.Safe.from_string |> member "fields"
+    |> member "description" |> to_string
+  in
+  Result.ok description
