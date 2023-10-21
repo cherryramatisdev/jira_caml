fed270929949b25b67970224ddaeb6d553315c4a
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-10-21 20:25:07 -0300

    refactor: rename cmd parameter to card_index

--- lib/commands/commands.ml
+++ lib/commands/commands.ml
@@ -1,32 +1,32 @@
-let view cmd =
+let view card_index =
   let open Lib__Shared in
   let open Lib__Jira in
-  let* description = get_card_content cmd in
+  let* description = get_card_content card_index in
   print_endline description;
   Result.ok description
 
-let progress cmd =
-  Printf.printf "progress command %s" cmd |> print_newline;
-  Result.ok cmd
+let progress card_index =
+  Printf.printf "progress command %s" card_index |> print_newline;
+  Result.ok card_index
 
-let review cmd =
-  Printf.printf "review command %s" cmd |> print_newline;
-  Result.ok cmd
+let review card_index =
+  Printf.printf "review command %s" card_index |> print_newline;
+  Result.ok card_index
 
 let open_card cmd =
   Printf.printf "open command %s" cmd |> print_newline;
   Result.ok cmd
 
-let status cmd =
+let status card_index =
   let open Lib__Shared in
   let open Lib__Jira in
-  let* status = get_card_status cmd in
+  let* status = get_card_status card_index in
   print_endline status;
   Result.ok status
 
-let pr_title cmd =
+let pr_title card_index =
   let open Lib__Shared in
   let open Lib__Jira in
-  let* title = get_card_title cmd in
+  let* title = get_card_title card_index in
   print_endline title;
   Result.ok title
