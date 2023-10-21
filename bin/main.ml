e8b9a8bc767104af604937680ca40e61da18cce9
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-10-21 00:47:07 -0300

    refactor: introduce plain old pattern matching
    I'm learn more about cmdliner, but for now this is plenty enough for me
    to move forward fast

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,42 @@
-let cmd =
-  let open Cmdliner in
-  let doc = "a CLI to manage common workflows with jira" in
-  let man =
-    [ `S Manpage.s_bugs; `P "Email bug reports to <bugs@example.org>" ]
-  in
-  let info = Cmd.info "jira" ~version:"0.0.1" ~doc ~man in
-  let open Commands in
-  Cmd.group info [pr_title_cmd; status_cmd; open_card_cmd; progress_cmd; review_cmd]
+let help_message =
+  "\n\
+   Available commands:\n\n\
+   - progress <code>: Send the jira card informed by the code to progress \
+   column and create a new branch for it\n\
+   - review <code>: Send the jira card informed by the code to review column \
+   and create a new pull request for it\n\
+   - status <code>: Print the current column for the jira card informed by the \
+   code\n\
+   - view <code>: Print the description for the jira card informed by the code\n\
+   - pr_title <code>: Print the title for the jira card informed by the code\n\
+   - open <code>: Open the current card informed by the code on the browser\n"
 
-let () = exit (Cmdliner.Cmd.eval cmd)
+let get_help () = print_endline help_message
+
+let parse_cmd_with_jira_code rest (cb : string -> (string, string) result) =
+  match rest with
+  | subcmd :: _ -> cb subcmd
+  | _ ->
+      get_help ();
+      Result.ok help_message
+
+let parse_cmd (cmd : string) (rest : string list) =
+  let open Commands in
+  match cmd with
+  | "progress" -> parse_cmd_with_jira_code rest progress
+  | "status" -> parse_cmd_with_jira_code rest status
+  | "view" -> parse_cmd_with_jira_code rest view
+  | "open" -> parse_cmd_with_jira_code rest open_card
+  | "review" -> parse_cmd_with_jira_code rest review
+  | "pr_title" -> parse_cmd_with_jira_code rest pr_title
+  | _ ->
+      get_help ();
+      Result.ok help_message
+
+let () =
+  let args = Array.to_list Sys.argv in
+  match args with
+  | [] -> get_help ()
+  | _ :: cmd :: rest -> (
+      match parse_cmd cmd rest with Ok _ -> () | Error _ -> get_help ())
+  | _ -> get_help ()
--- lib/commands/commands.ml
+++ lib/commands/commands.ml
@@ -1,37 +1,29 @@
-let mount_cmd ~doc ~name ~cmd =
-  let open Cmdliner in
-  let info = Cmd.info name ~doc in
-  let term = Term.(const cmd $ const ()) in
-  Cmd.v info term
+let view cmd =
+  let open Lib__Shared in
+  let open Lib__Jira in
+  let* description = get_card_content cmd in
+  print_endline description;
+  Result.ok description
 
-let pr_title () = print_endline "pr_title command"
-let status () = print_endline "status command"
-let view () = print_endline "view command"
-let open_card () = print_endline "open_card command"
-let progress () = print_endline "progress command"
-let review () = print_endline "review command"
+let progress cmd =
+  Printf.printf "progress command %s" cmd |> print_newline;
+  Result.ok cmd
 
-let pr_title_cmd =
-  mount_cmd
-    ~doc:
-      "This command print out the title of the current card following a github \
-       pull request pattern"
-    ~name:"pr_title" ~cmd:pr_title
+let review cmd =
+  Printf.printf "review command %s" cmd |> print_newline;
+  Result.ok cmd
 
-let status_cmd =
-  mount_cmd ~doc:"This command print out which column the current card is"
-    ~name:"status" ~cmd:status
+let open_card cmd =
+  Printf.printf "open command %s" cmd |> print_newline;
+  Result.ok cmd
 
-let open_card_cmd =
-  mount_cmd ~doc:"This command open the current card on the available browser"
-    ~name:"open" ~cmd:open_card
+let status cmd =
+  Printf.printf "status command %s" cmd |> print_newline;
+  Result.ok cmd
 
-let progress_cmd =
-  mount_cmd
-    ~doc:"This command move a particular jira card to\nthe progress column"
-    ~name:"progress" ~cmd:progress
-
-let review_cmd =
-  mount_cmd
-    ~doc:"This command move a particular jira card to\nthe review column"
-    ~name:"review" ~cmd:review
+let pr_title cmd =
+  let open Lib__Shared in
+  let open Lib__Jira in
+  let* title = get_card_title cmd in
+  print_endline title;
+  Result.ok title
