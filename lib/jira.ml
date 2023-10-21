13171568f68f9b1a004ba96a915a54c45515d47d
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-10-21 00:47:49 -0300

    feat: improve jira module
    Abstracting some functions to make easier to add new ones

--- lib/jira.ml
+++ lib/jira.ml
@@ -1,19 +1,34 @@
-let get_card_content code =
+let get_authentication_headers (config : Config.t) =
   let open Shared in
   let open Cohttp in
+  Header.init () |> fun h ->
+  Header.add_authorization h
+    (`Basic (config.auth.user_mail, config.auth.user_token))
+
+let get_card code =
+  let open Shared in
   let* config = Config.config () in
-  let headers =
-    Header.init () |> fun h ->
-    Header.add_authorization h
-      (`Basic (config.auth.user_mail, config.auth.user_token))
-  in
+  let headers = get_authentication_headers config in
   let url =
     Printf.sprintf "%s/rest/api/2/issue/%s-%s" config.prefixes.url_prefix
       config.prefixes.card_prefix code
   in
+  Result.ok (Http.get ~headers url |> Yojson.Safe.from_string)
+
+let get_card_content code =
+  let open Shared in
   let open Yojson.Safe.Util in
+  let* card_content = get_card code in
   let description =
-    Http.get ~headers url |> Yojson.Safe.from_string |> member "fields"
-    |> member "description" |> to_string
+    card_content |> member "fields" |> member "description" |> to_string
   in
   Result.ok description
+
+let get_card_title code =
+  let open Shared in
+  let open Yojson.Safe.Util in
+  let* card_content = get_card code in
+  let title =
+    card_content |> member "fields" |> member "summary" |> to_string
+  in
+  Result.ok title
