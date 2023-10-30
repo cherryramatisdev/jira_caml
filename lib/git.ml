d87aac0980abc3703733bb46492a9242773f3d69
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-10-30 18:54:31 -0300

    fix: use the correct git command to get the current branch

--- lib/git.ml
+++ lib/git.ml
@@ -1,9 +1,9 @@
 let extract_jira_card_from_git_branch prefix =
-  let open Str in
   let open Shared in
+  let open Str in
   let branch_name =
-    execute_and_capture_output "git branch" |> fun str ->
-    remove_char str '*' |> String.trim
+    Shell.execute_and_capture_output "git rev-parse --abbrev-ref HEAD"
+    |> fun str -> str |> String.trim
   in
   let regex = Printf.sprintf ".*%s-\\([0-9]+\\)" prefix |> regexp in
   match string_match regex branch_name 0 with
